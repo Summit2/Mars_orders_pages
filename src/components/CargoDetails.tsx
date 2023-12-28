1b42dda693863d27ebab6ca5c4489e913cb8a178
Summit2 <pinkslime987@gmail.com>
2023-12-28 15:10:12 +0300

    adad

--- src/components/CargoDetails.tsx
+++ src/components/CargoDetails.tsx
@@ -44,7 +44,7 @@ const CargoDetails: React.FC = () => {
     <>
       {cargoItem.title}
       <div className="current-image">
-      <img src={`data:image/jpeg;base64,${cargoItem.image_binary.toString()}`} style={{ height: '300px', width: '400px' }} />
+      <img src={`data:image/jpeg;base64,${cargoItem.image_binary.toString()}`} style={{ height: '500px', width: '600px' }} />
 
       </div>
       <div className='font'>
--- src/components/CargoList.tsx
+++ src/components/CargoList.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
-// import axios from 'axios';
 import { useLocation, useNavigate } from 'react-router-dom';
-import {mock_data } from './data.ts'
+import { mock_data } from './data.ts';
 
 interface CargoItem {
   pk: number;
@@ -14,60 +13,42 @@ interface CargoItem {
 }
 
 const CargoList = () => {
-  // @ts-ignore
   const [data, setData] = useState<CargoItem[]>([]);
-  // @ts-ignore
   const [loading, setLoading] = useState(true);
-  // @ts-ignore
   const [error, setError] = useState<string | null>(null);
-  // @ts-ignore
   const location = useLocation();
-  // @ts-ignore
   const navigate = useNavigate();
 
-// @ts-ignore
-  const fetchData = async (filter: string | null = null) => {
-    // try {
-    //   let url = "http://localhost:8000/cargo/";
+  const filterData = (filter: string) => {
+    const filteredData = mock_data.filter(item => item.title.includes(filter));
+    return filteredData;
+  };
 
-    //   // Append filter to the URL if it's provided
-    //   // if (filter) {
-    //   //   url += `?filter=${filter}`;
-    //   // }
+  const fetchData = (filter: string | null = null) => {
+    try {
+      const filteredData = filter ? filterData(filter) : mock_data;
 
-    //   const response = await axios.get(url);
-    //   setData(response.data.data);
-    // } catch (error) {
-    //   console.error("Error fetching data:", error);
-    //   setError("An error occurred while fetching data.");
-
-    // @ts-ignore
-      setData(mock_data);
-      console.log(mock_data)
+      // @ts-ignore
+      setData(filteredData);
       setLoading(false);
       setError(null);
-      
-      
-    
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("An error occurred while fetching data.");
+      setLoading(false);
+    }
   };
 // @ts-ignore
   const handleFilter = (filter: string) => {
-    // Perform a PUT request to update the filter on the server
-    
-    
-    // @ts-ignore
-      
-        // After the PUT request is successful, trigger the GET request
-        fetchData(mock_data);
-        // Update the URL with the selected filter
-        navigate(`/cargo?filter=${filter}`);
-      
-      
+    fetchData(filter);
+    navigate(`/cargo?filter=${filter}`);
   };
-  
 
   useEffect(() => {
-    fetchData();
+    const urlSearchParams = new URLSearchParams(location.search);
+    const filterParam = urlSearchParams.get('filter');
+    
+    fetchData(filterParam);
   }, [location.search]);
 
   if (loading) {
@@ -78,13 +59,10 @@ const CargoList = () => {
     return <p>{error}</p>;
   }
 
-
-
   return (
     <ul className='card-list'>
       {Array.isArray(data) && data.map((item) => (
         <li key={item.pk} className="card">
-          
           <div className="delete-button">
             <form method="post" action="deleteCargo/">
               <input type="hidden" name="id_del" value={item.pk} />
@@ -93,13 +71,9 @@ const CargoList = () => {
               </button>
             </form>
           </div>
-
           <div className="font-ordinary">{item.title}</div>
-          
-            <img src={`data:image/jpeg;base64,${item.image_binary.toString('base64')}`} className="images" alt={item.title} />
-          
+          <img src={`data:image/jpeg;base64,${item.image_binary.toString('base64')}`} className="images" alt={item.title} />
           <br />
-
           <a href={`/Mars_orders_pages/#/cargo/${item.pk}`} className="beautiful-link">
             <h2>Подробнее...</h2>
           </a>
