72e0ff9e80e270e161d1f6977f83f119a0d48d08
Summit2 <pinkslime987@gmail.com>
2023-12-28 14:07:51 +0300

    dsdms

--- src/components/CargoList.tsx
+++ src/components/CargoList.tsx
@@ -22,19 +22,19 @@ const CargoList = () => {
 
 // @ts-ignore
   const fetchData = async (filter: string | null = null) => {
-    try {
-      let url = "http://localhost:8000/cargo/";
+    // try {
+    //   let url = "http://localhost:8000/cargo/";
 
-      // Append filter to the URL if it's provided
-      // if (filter) {
-      //   url += `?filter=${filter}`;
-      // }
+    //   // Append filter to the URL if it's provided
+    //   // if (filter) {
+    //   //   url += `?filter=${filter}`;
+    //   // }
 
-      const response = await axios.get(url);
-      setData(response.data.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setError("An error occurred while fetching data.");
+    //   const response = await axios.get(url);
+    //   setData(response.data.data);
+    // } catch (error) {
+    //   console.error("Error fetching data:", error);
+    //   setError("An error occurred while fetching data.");
 
     // @ts-ignore
       setData(mock_data);
